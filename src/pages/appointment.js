fe3644bdd9befb8ee6a7dd0a75bd2b55c59db2b7
surya <suryagopinathan@gmail.com>
2020-11-18 23:29:04 +0530

    have to fix color changing but works just fine

--- src/pages/appointment.js
+++ src/pages/appointment.js
@@ -4,9 +4,9 @@ import Calendar from 'react-calendar';
 import '../style/appointment.css';
 import Back from '../components/back_btn'
 import Docsel from '../components/doc_select'
-  var time=["09:00 am","10:00 am","11:00 am","12:00 pm","01:00 pm","02:00 pm","03:00 pm","04:00 pm","05:00 pm","06:00 pm"];
+  var time=["09:00 am","10:00 am","11:00 am","12:00 pm","01:00 pm","02:00 pm","03:00 pm","04:00 pm","05:00 pm","06:00 pm"];//12 hr format for displaying
   const day=["sun","mon","tue","wed","thu","fri","sat"];
-  var data =  [
+  /*var data =  [
     ["01-02-2020","02-02-2020","03-02-2020","04-02-2020","05-02-2020","06-02-2020","07-02-2020"],
     ["mon","tue","wed","thu","fri","sat","sun"],
     [1,0,1,0,1,0,1],
@@ -19,7 +19,22 @@ import Docsel from '../components/doc_select'
     [0,1,0,1,0,1,0],
     [1,0,1,0,1,0,1],
     [0,1,0,1,0,1,0]
+  ];*/
+  var data =  [
+    ["01-02-2020","02-02-2020","03-02-2020","04-02-2020","05-02-2020","06-02-2020","07-02-2020"],
+    ["mon","tue","wed","thu","fri","sat","sun"],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0]
   ];
+  var timings=["9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00"];//server 24 hr format
   var doclist=Array();
   doclist=[{
     "n": " ",
@@ -52,24 +67,42 @@ import Docsel from '../components/doc_select'
             }
     })
     }
-    get_date(lpdate){
+    get_date(lpdate,i){
         var ret={};
         //post values
         var month=lpdate.getMonth()+1
-        var date=lpdate.getDate()+'-'+month;
+        var getdate=lpdate.getDate()+'-'+month;
         var dateyear=String(lpdate.getFullYear());
         var doc=this.state.docselect;
-            axios.post(`https://bqhdj6kx2j.execute-api.ap-south-1.amazonaws.com/test/getdate`,{date:date,year:dateyear,doc:doc}).then(res => { 
-                console.log(lpdate+"  "+res.data);    
+        //console.log("------"+doc+" "+dateyear+" "+getdate);
+            axios.post(`https://bqhdj6kx2j.execute-api.ap-south-1.amazonaws.com/test/getdate`,{date:getdate,year:dateyear,doc:doc}).then(res => {   
                 if(res.data["message"]!="Internal server error"){
                     ret=res.data;
+                    console.log("response: "+i+"  "+JSON.stringify(res.data));
+                    for(var x in res.data) {
+                        var slot=res.data[x];
+                        console.log("res body "+slot);
+                        for(var y in timings)
+                        if(String(timings[y]) in slot){
+                            console.log(String(timings[y])+" oclock is "+ slot[timings[y]]);
+                            console.log("y value"+y);   
+                            //[ a- available ]      [ b- booked ]       [ c- cancelled(holiday/doc leave) ]
+                            if(slot[timings[y]]=='a'){
+                                data[parseInt(y)+2][i]=1;
+                                //console.log("data["+(parseInt(y)+2)+"]["+i+"]");
+                            }else if(slot[timings[y]]=='b'){
+                                data[parseInt(y)+2][i]=1    ;
+                            }
+                        }
+
+                    }
+
                 }
                 })
-        return ret;
     }
     get_doc(e){
         axios.get(`https://u69ys2399d.execute-api.ap-south-1.amazonaws.com/test`,{}).then(res => {
-            console.log(res.data);
+            //console.log(res.data);
           if(res.data["message"]!="Internal server error"){
               doclist=res.data;
               doc[0]=doclist[0]['n'];
@@ -80,13 +113,28 @@ import Docsel from '../components/doc_select'
         })
     }
     selectcallback = (childData) => {
-        console.log("childdata: "+childData);
+        //console.log("childdata: "+childData);
         this.setState({docselect:parseInt(childData)});
         this.setState({docname:doclist[childData]})
         doc[0]=doclist[parseInt(childData)]['n'];
-        console.log("selected doctor: "+doclist[childData]['id']);
+        //console.log("selected doctor: "+doclist[childData]['id']);
         this.get_desc(doclist[childData]['id']);
 
+    }
+    slotcolor(ind){
+        var ret="appt-c";
+        switch(ind){
+            case 1:
+               ret="appt-a";
+               break;
+           case 2:
+               ret="appt-b";
+               break;
+           case 3:
+               ret="appt-c";
+               break;
+        }
+        return ret;
     }
     renderTable() {
         return data.map((dat, index) => {
@@ -130,6 +178,7 @@ import Docsel from '../components/doc_select'
            return ret
         })
      }
+
      togglePop = () => {
          this.setState({pop:datpop});
         datpop=!datpop;
@@ -139,18 +188,12 @@ import Docsel from '../components/doc_select'
        var lpdate=new Date;
        lpdate=date;
        lpdate.setDate(date.getDate()-3);
-       
        for(var i=0;i<=6;i++){
            data[0][i]=lpdate.getDate()+'-'+(lpdate.getMonth()+1)+'-'+lpdate.getFullYear();
            data[1][i]=day[lpdate.getDay()];
            //get timings per day
-            var slots=this.get_date(date);
-            console.log("slots is "+JSON.stringify(slots));
-            for (i in slots) {
-            //if(slots)
-            }
-
-            console.log("loop "+lpdate);
+            this.get_date(lpdate,i);
+            //loop adding date
            lpdate.setDate(lpdate.getDate()+1);
            
         }
