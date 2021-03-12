5eaf9ae5ee355465a3a80d8c675729ca4a22351c
surya <suryagopinathan@gmail.com>
2021-03-12 22:52:18 +0530

    removing redundant file

--- src/components/back_btn.js
+++ src/components/back_btn.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import history from './history';
-import '../style/backbtn.css'
+import styles from '../style/global.scss';
 const styl = {
     padding: '0 10px 0 30px',
   };
 const Back = () =>{
        return (<div>
-        <button className="bck" onClick={() => history.goBack()}
+        <button className={styles.bck} onClick={() => history.goBack()}
          style={styl}
         > &laquo; Back</button>
         </div>);
